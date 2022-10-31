657a58be4e75340f022676a8b1bc6f40175bd8ed
TicTacToe333 <97261933+TicTacToe333@users.noreply.github.com>
2022-10-31 11:21:03 -0600

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,192 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.tiptime
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.EditText
+
+data class Recipe(
+    val name: String,
+    val ingredients: String)
+
+class Datasource() {
+    private var list = mutableListOf<Recipe>()
+    fun loadRecipe(): MutableList<Recipe>{
+        return list
+    }
+    fun addRecipe(recipe: Recipe){
+        list.add(recipe)
+    }
+
+    fun findRecipe(name: String, ingredients: String){
+        if (list.indexOf(Recipe(name, ingredients)) != -1) {
+                print("hi")
+        }
+        print("test")
+    }
+
+    fun printList(){
+        for (element in list) {
+            print(element)
+        }
+    }
+
+    fun getList(): String{
+        return " "
+        //return list[0].component1()
+    }
+}
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipTimeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    TipTimeScreen()
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun TipTimeScreen() {
+    var name by remember { mutableStateOf("") }
+    var ingredient by remember { mutableStateOf("") }
+    var title = "Recipe Logger"
+    var list = Datasource()
+
+    list.addRecipe(Recipe("food", "ingred"))
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+
+    ) {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(Modifier.height(16.dp))
+        EditText(
+            value = name,
+            onValueChanged = { name = it },
+            label = "Name of dish"
+        )
+        Spacer(Modifier.height(16.dp))
+        EditText(
+            value = ingredient,
+            onValueChanged = { ingredient = it },
+            label = "Ingredients"
+        )
+        Spacer(Modifier.height(24.dp))
+        Button(onClick = { list.addRecipe(Recipe(name, ingredient))}){
+            Text("Submit new recipe")
+        }
+
+        Button(onClick = {list.findRecipe(name, ingredient)}){
+            Text("Find recipe")
+        }
+        Button(onClick = {list.printList()}){
+            Text("All Recipes")
+        }
+
+        Spacer(Modifier.height(16.dp))
+        Text(
+            text = list.getList()
+        )
+    }
+}
+
+@Composable
+fun test(name: String, ingredients: String){
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+
+    ) {
+        Text(
+            text = "sucess",
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+
+@Composable
+fun EditText(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    label: String
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChanged,
+        label = { Text(label) },
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+    )
+
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun TipTimeScreenPreview() {
+    TipTimeTheme {
+        TipTimeScreen()
+    }
+}
+
